c1d0364b76cc4bd19e16e6bf9fe1a58aa0dd2302
pavand96 <pavandnitt96@gmail.com>
2026-05-01 18:10:45 -0400

    Rename gearbox sizing parameters

--- gearbox/gearbox.sv
+++ gearbox/gearbox.sv
@@ -5,20 +5,20 @@ module gearbox #(
   localparam IN_DW  = 8 * IN_DB,
   localparam OUT_DW = 8 * OUT_DB,
 
-  localparam int MAX_DB = (IN_DB > OUT_DB) ? IN_DB : OUT_DB,
-  localparam int BUF_DB = 2 * MAX_DB,
-  localparam int BUF_DW = 8 * BUF_DB,
+  localparam int MAX_TRANSFER_BYTES    = (IN_DB > OUT_DB) ? IN_DB : OUT_DB,
+  localparam int BUFFER_CAPACITY_BYTES = 2 * MAX_TRANSFER_BYTES,
+  localparam int BUFFER_DATA_WIDTH     = 8 * BUFFER_CAPACITY_BYTES,
 
-  localparam int PTR_W = $clog2(BUF_DB),
-  localparam int CNT_W = $clog2(BUF_DB + 1),
+  localparam int BUFFER_POINTER_WIDTH = $clog2(BUFFER_CAPACITY_BYTES),
+  localparam int BUFFER_COUNT_WIDTH   = $clog2(BUFFER_CAPACITY_BYTES + 1),
 
-  localparam logic [PTR_W-1:0] IN_DB_PTR  = IN_DB,
-  localparam logic [PTR_W-1:0] OUT_DB_PTR = OUT_DB,
-  localparam logic [PTR_W:0]   BUF_DB_PTR = BUF_DB,
+  localparam logic [BUFFER_POINTER_WIDTH-1:0] INPUT_POINTER_STEP   = IN_DB,
+  localparam logic [BUFFER_POINTER_WIDTH-1:0] OUTPUT_POINTER_STEP  = OUT_DB,
+  localparam logic [BUFFER_POINTER_WIDTH:0]   BUFFER_POINTER_LIMIT = BUFFER_CAPACITY_BYTES,
 
-  localparam logic [CNT_W-1:0] IN_DB_CNT  = IN_DB,
-  localparam logic [CNT_W-1:0] OUT_DB_CNT = OUT_DB,
-  localparam logic [CNT_W-1:0] BUF_DB_CNT = BUF_DB
+  localparam logic [BUFFER_COUNT_WIDTH-1:0] INPUT_BYTE_COUNT  = IN_DB,
+  localparam logic [BUFFER_COUNT_WIDTH-1:0] OUTPUT_BYTE_COUNT = OUT_DB,
+  localparam logic [BUFFER_COUNT_WIDTH-1:0] BUFFER_BYTE_COUNT = BUFFER_CAPACITY_BYTES
 )
 (
   input valid_in,
@@ -36,14 +36,14 @@ module gearbox #(
   logic in_data_moving;
   logic out_data_moving;
 
-  logic [CNT_W-1:0] stored_bytes_q;
-  logic [CNT_W-1:0] stored_bytes_in;
+  logic [BUFFER_COUNT_WIDTH-1:0] stored_bytes_q;
+  logic [BUFFER_COUNT_WIDTH-1:0] stored_bytes_in;
 
-  logic [CNT_W-1:0] bytes_inc;
-  logic [CNT_W-1:0] bytes_dec;
+  logic [BUFFER_COUNT_WIDTH-1:0] bytes_inc;
+  logic [BUFFER_COUNT_WIDTH-1:0] bytes_dec;
 
-  logic [CNT_W:0] stored_bytes_calc;
-  logic [CNT_W:0] free_bytes_after_read;
+  logic [BUFFER_COUNT_WIDTH:0] stored_bytes_calc;
+  logic [BUFFER_COUNT_WIDTH:0] free_bytes_after_read;
 
   assign in_data_moving =
       valid_in
@@ -54,7 +54,7 @@ module gearbox #(
     & ready_in;
 
   assign free_bytes_after_read =
-      {1'b0, BUF_DB_CNT}
+      {1'b0, BUFFER_BYTE_COUNT}
     - {1'b0, stored_bytes_q}
     + {1'b0, bytes_dec};
 
@@ -64,7 +64,7 @@ module gearbox #(
     - {1'b0, bytes_dec};
 
   assign stored_bytes_in =
-    stored_bytes_calc[CNT_W-1:0];
+    stored_bytes_calc[BUFFER_COUNT_WIDTH-1:0];
 
   always_ff @(posedge clk or negedge rstn) begin
     if(~rstn)
@@ -72,6 +72,21 @@ module gearbox #(
     else
       stored_bytes_q <= stored_bytes_in;
   end
+
+`ifndef SYNTHESIS
+  always_ff @(posedge clk) begin
+    if(rstn) begin
+      assert (stored_bytes_q <= BUFFER_BYTE_COUNT)
+        else $error("gearbox stored byte count exceeded buffer capacity");
+
+      assert ({1'b0, bytes_dec} <= {1'b0, stored_bytes_q})
+        else $error("gearbox attempted to remove more bytes than stored");
+
+      assert (stored_bytes_calc <= {1'b0, BUFFER_BYTE_COUNT})
+        else $error("gearbox next stored byte count exceeded buffer capacity");
+    end
+  end
+`endif
 
   if(IN_DB < OUT_DB) begin : g_small_to_large
 
@@ -83,15 +98,15 @@ module gearbox #(
 
     logic [IN_DW-1:0] pack_data_q;
 
-    logic [BUF_DW-1:0] packrot_data_q;
-    logic [BUF_DW-1:0] packrot_data_in;
-    logic [BUF_DW-1:0] packrot_write_data;
-    logic [BUF_DW-1:0] packrot_write_mask;
+    logic [BUFFER_DATA_WIDTH-1:0] packrot_data_q;
+    logic [BUFFER_DATA_WIDTH-1:0] packrot_data_in;
+    logic [BUFFER_DATA_WIDTH-1:0] packrot_write_data;
+    logic [BUFFER_DATA_WIDTH-1:0] packrot_write_mask;
 
-    logic [PTR_W-1:0] wr_ptr_q;
-    logic [PTR_W-1:0] wr_ptr_in;
-    logic [PTR_W:0] wr_ptr_sum;
-    logic [PTR_W:0] wr_ptr_wrap;
+    logic [BUFFER_POINTER_WIDTH-1:0] wr_ptr_q;
+    logic [BUFFER_POINTER_WIDTH-1:0] wr_ptr_in;
+    logic [BUFFER_POINTER_WIDTH:0] wr_ptr_sum;
+    logic [BUFFER_POINTER_WIDTH:0] wr_ptr_wrap;
 
     logic rd_lane_q;
     logic rd_lane_in;
@@ -102,15 +117,15 @@ module gearbox #(
       : packrot_data_q[OUT_DW-1:0];
 
     assign valid_out =
-      stored_bytes_q >= OUT_DB_CNT;
+      stored_bytes_q >= OUTPUT_BYTE_COUNT;
 
     assign bytes_dec =
         out_data_moving
-      ? OUT_DB_CNT
+      ? OUTPUT_BYTE_COUNT
       : '0;
 
     assign packrot_has_space =
-      free_bytes_after_read >= {1'b0, IN_DB_CNT};
+      free_bytes_after_read >= {1'b0, INPUT_BYTE_COUNT};
 
     assign ready_out =
         ~pack_data_valid_q
@@ -127,42 +142,42 @@ module gearbox #(
 
     assign bytes_inc =
         pack_data_moving
-      ? IN_DB_CNT
+      ? INPUT_BYTE_COUNT
       : '0;
 
     assign wr_ptr_sum =
         {1'b0, wr_ptr_q}
-      + {1'b0, IN_DB_PTR};
+      + {1'b0, INPUT_POINTER_STEP};
 
     assign wr_ptr_wrap =
         wr_ptr_sum
-      - BUF_DB_PTR;
+      - BUFFER_POINTER_LIMIT;
 
     assign wr_ptr_in =
         pack_data_moving
-      ? ((wr_ptr_sum >= BUF_DB_PTR)
-        ? wr_ptr_wrap[PTR_W-1:0]
-        : wr_ptr_sum[PTR_W-1:0])
+      ? ((wr_ptr_sum >= BUFFER_POINTER_LIMIT)
+        ? wr_ptr_wrap[BUFFER_POINTER_WIDTH-1:0]
+        : wr_ptr_sum[BUFFER_POINTER_WIDTH-1:0])
       : wr_ptr_q;
 
     assign rd_lane_in =
         rd_lane_q
       ^ out_data_moving;
 
-    for(genvar out_byte_i = 0; out_byte_i < BUF_DB; out_byte_i = out_byte_i + 1) begin : g_packrot_out_byte
+    for(genvar out_byte_i = 0; out_byte_i < BUFFER_CAPACITY_BYTES; out_byte_i = out_byte_i + 1) begin : g_packrot_out_byte
 
-      localparam logic [PTR_W-1:0] OUT_BYTE_PTR = out_byte_i;
+      localparam logic [BUFFER_POINTER_WIDTH-1:0] OUT_BYTE_PTR = out_byte_i;
 
       logic [IN_DB-1:0] write_byte_sel;
       logic [7:0] [IN_DB-1:0] write_bit_sel;
 
       for(genvar in_byte_i = 0; in_byte_i < IN_DB; in_byte_i = in_byte_i + 1) begin : g_pack_data_in_byte
 
-        localparam logic [PTR_W:0] IN_BYTE_OFFSET = in_byte_i;
+        localparam logic [BUFFER_POINTER_WIDTH:0] IN_BYTE_OFFSET = in_byte_i;
 
-        logic [PTR_W:0] write_byte_sum;
-        logic [PTR_W:0] write_byte_wrap;
-        logic [PTR_W-1:0] write_byte_ptr;
+        logic [BUFFER_POINTER_WIDTH:0] write_byte_sum;
+        logic [BUFFER_POINTER_WIDTH:0] write_byte_wrap;
+        logic [BUFFER_POINTER_WIDTH-1:0] write_byte_ptr;
 
         assign write_byte_sum =
             {1'b0, wr_ptr_q}
@@ -170,12 +185,12 @@ module gearbox #(
 
         assign write_byte_wrap =
             write_byte_sum
-          - BUF_DB_PTR;
+          - BUFFER_POINTER_LIMIT;
 
         assign write_byte_ptr =
-            write_byte_sum >= BUF_DB_PTR
-          ? write_byte_wrap[PTR_W-1:0]
-          : write_byte_sum[PTR_W-1:0];
+            write_byte_sum >= BUFFER_POINTER_LIMIT
+          ? write_byte_wrap[BUFFER_POINTER_WIDTH-1:0]
+          : write_byte_sum[BUFFER_POINTER_WIDTH-1:0];
 
         assign write_byte_sel[in_byte_i] =
           write_byte_ptr == OUT_BYTE_PTR;
@@ -241,16 +256,16 @@ module gearbox #(
     logic [OUT_DW-1:0] out_stage_data_in;
     logic [OUT_DW-1:0] barrel_data_out;
 
-    logic [BUF_DW-1:0] unpack_data_q;
-    logic [BUF_DW-1:0] unpack_data_in;
+    logic [BUFFER_DATA_WIDTH-1:0] unpack_data_q;
+    logic [BUFFER_DATA_WIDTH-1:0] unpack_data_in;
 
     logic wr_lane_q;
     logic wr_lane_in;
 
-    logic [PTR_W-1:0] rd_ptr_q;
-    logic [PTR_W-1:0] rd_ptr_in;
-    logic [PTR_W:0] rd_ptr_sum;
-    logic [PTR_W:0] rd_ptr_wrap;
+    logic [BUFFER_POINTER_WIDTH-1:0] rd_ptr_q;
+    logic [BUFFER_POINTER_WIDTH-1:0] rd_ptr_in;
+    logic [BUFFER_POINTER_WIDTH:0] rd_ptr_sum;
+    logic [BUFFER_POINTER_WIDTH:0] rd_ptr_wrap;
 
     assign valid_out =
        out_stage_valid_q;
@@ -263,7 +278,7 @@ module gearbox #(
       | ready_in;
 
     assign buffer_output_valid =
-      stored_bytes_q >= OUT_DB_CNT;
+      stored_bytes_q >= OUTPUT_BYTE_COUNT;
 
     assign buffer_output_moving =
         buffer_output_valid
@@ -284,15 +299,15 @@ module gearbox #(
 
     assign bytes_dec =
         buffer_output_moving
-      ? OUT_DB_CNT
+      ? OUTPUT_BYTE_COUNT
       : '0;
 
     assign ready_out =
-      free_bytes_after_read >= {1'b0, IN_DB_CNT};
+        free_bytes_after_read >= {1'b0, INPUT_BYTE_COUNT};
 
     assign bytes_inc =
         in_data_moving
-      ? IN_DB_CNT
+      ? INPUT_BYTE_COUNT
       : '0;
 
     assign wr_lane_in =
@@ -308,29 +323,29 @@ module gearbox #(
 
     assign rd_ptr_sum =
         {1'b0, rd_ptr_q}
-      + {1'b0, OUT_DB_PTR};
+      + {1'b0, OUTPUT_POINTER_STEP};
 
     assign rd_ptr_wrap =
         rd_ptr_sum
-      - BUF_DB_PTR;
+      - BUFFER_POINTER_LIMIT;
 
     assign rd_ptr_in =
         buffer_output_moving
-      ? (( rd_ptr_sum >= BUF_DB_PTR)
-         ? rd_ptr_wrap[PTR_W-1:0]
-         : rd_ptr_sum[PTR_W-1:0])
+      ? (( rd_ptr_sum >= BUFFER_POINTER_LIMIT)
+         ? rd_ptr_wrap[BUFFER_POINTER_WIDTH-1:0]
+         : rd_ptr_sum[BUFFER_POINTER_WIDTH-1:0])
       : rd_ptr_q;
 
     for(genvar out_byte_i = 0; out_byte_i < OUT_DB; out_byte_i = out_byte_i + 1) begin : g_barrel_out_byte
 
-      localparam logic [PTR_W:0] OUT_BYTE_OFFSET = out_byte_i;
+      localparam logic [BUFFER_POINTER_WIDTH:0] OUT_BYTE_OFFSET = out_byte_i;
 
-      logic [PTR_W:0] read_byte_sum;
-      logic [PTR_W:0] read_byte_wrap;
-      logic [PTR_W-1:0] read_byte_ptr;
+      logic [BUFFER_POINTER_WIDTH:0] read_byte_sum;
+      logic [BUFFER_POINTER_WIDTH:0] read_byte_wrap;
+      logic [BUFFER_POINTER_WIDTH-1:0] read_byte_ptr;
 
-      logic [BUF_DB-1:0] buffer_byte_sel;
-      logic [BUF_DB-1:0] buffer_bit_sel [7:0];
+      logic [BUFFER_CAPACITY_BYTES-1:0] buffer_byte_sel;
+      logic [BUFFER_CAPACITY_BYTES-1:0] buffer_bit_sel [7:0];
 
       assign read_byte_sum =
           {1'b0, rd_ptr_q}
@@ -338,16 +353,16 @@ module gearbox #(
 
       assign read_byte_wrap =
           read_byte_sum
-        - BUF_DB_PTR;
+        - BUFFER_POINTER_LIMIT;
 
       assign read_byte_ptr =
-          (read_byte_sum >= BUF_DB_PTR)
-        ? read_byte_wrap[PTR_W-1:0]
-        : read_byte_sum[PTR_W-1:0];
+          (read_byte_sum >= BUFFER_POINTER_LIMIT)
+        ? read_byte_wrap[BUFFER_POINTER_WIDTH-1:0]
+        : read_byte_sum[BUFFER_POINTER_WIDTH-1:0];
 
-      for(genvar buf_byte_i = 0; buf_byte_i < BUF_DB; buf_byte_i = buf_byte_i + 1) begin : g_buffer_byte
+      for(genvar buf_byte_i = 0; buf_byte_i < BUFFER_CAPACITY_BYTES; buf_byte_i = buf_byte_i + 1) begin : g_buffer_byte
 
-        localparam logic [PTR_W-1:0] BUFFER_BYTE_PTR = buf_byte_i;
+        localparam logic [BUFFER_POINTER_WIDTH-1:0] BUFFER_BYTE_PTR = buf_byte_i;
 
         assign buffer_byte_sel[buf_byte_i] =
           read_byte_ptr == BUFFER_BYTE_PTR;
